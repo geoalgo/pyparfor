d83a2270ff018646daa9a23543d151ea33e0e9d3
David  Salinas <david.salinas.pro@gmail.com>
2024-09-24 16:31:51 +0200

    support empty context & doc

--- parfor/__init__.py
+++ parfor/__init__.py
@@ -1,27 +1,31 @@
 from tqdm import tqdm
 from typing import TypeVar, Callable, List, Union
+
 # Test ci
 A = TypeVar('A')
 B = TypeVar('B')
 
+
 def parfor(
-    f: Callable[[object], B],
-    inputs: List[Union[list, dict]],
-    context: dict,
-    engine: str = "ray",
-) -> List[B]:
+        f: Callable[[object], B],
+        inputs: list[list | dict],
+        context: dict | None = None,
+        engine: str = "ray",
+) -> list[B]:
     """
-    Evaluates an embarrasingly parallel for-loop.
+    Evaluates an embarrassingly parallel for-loop.
     :param f: the function to be evaluated, the function is evaluated on `f(x, **context)` for all `x` in `inputs`
     if inputs are a list and on the union of x and context keyword arguments else
     :param inputs: list of inputs to be evaluated
     :param context: additional constant arguments to be passed to `f`. When using ray, the context is put in the local
      object store which avoids serializing multiple times, when using joblib, the context is serialized for each input.
-    :param engine: can be ["sequential", "ray", "joblib"]
+    :param engine: can be ["sequential", "ray", "joblib", "futures"]
     :return: a list where the function is evaluated on all inputs together with the context, i.e.
     `[f(x, **context) for x in inputs]`.
     """
-    assert engine in ["sequential", "ray", "joblib"]
+    assert engine in ["sequential", "ray", "joblib", "futures"]
+    if context is None:
+        context = {}
     if engine == "sequential":
         return [
             f(**x, **context) if isinstance(x, dict) else f(*x, **context)
@@ -33,13 +37,19 @@ def parfor(
             delayed(f)(**x, **context) if isinstance(x, dict) else delayed(f)(*x, **context)
             for x in inputs
         )
+    if engine == "futures":
+        from concurrent.futures import ThreadPoolExecutor
+        with ThreadPoolExecutor() as executor:
+            return list(executor.map(lambda x: f(**x, **context) if isinstance(x, dict) else f(*x, **context), inputs))
     if engine == "ray":
         import ray
         if not ray.is_initialized():
             ray.init()
+
         @ray.remote
         def remote_f(x, context):
             return f(**x, **context) if isinstance(x, dict) else f(*x, **context)
+
         remote_context = ray.put(context)
         remote_results = [remote_f.remote(x, remote_context) for x in inputs]
         return [ray.get(res) for res in tqdm(remote_results)]
--- tst/test_parfor.py
+++ tst/test_parfor.py
@@ -4,14 +4,25 @@ from parfor import parfor
 def f(x, v1, v2):
     return x + v1 + v2
 
+engines = [
+    "sequential",
+    "joblib",
+    "futures",
+    # TODO fix me in CI
+    # "ray"
+]
 
-def test_parallel_for_list_arguments():
-    for engine in [
-        "sequential",
-        "joblib",
-        # TODO fix me in CI
-        # "ray"
-    ]:
+def test_empty_context():
+    for engine in engines:
+        res = parfor(
+            f, [[1, 2, 3], [2, 2, 3], [3, 2, 3]], engine=engine
+        )
+        print(res)
+        assert res == [6, 7, 8]
+
+
+def test__list_arguments():
+    for engine in engines:
         res = parfor(
             f, [[1], [2], [3]], context=dict(v1=2, v2=3), engine=engine
         )
@@ -19,13 +30,8 @@ def test_parallel_for_list_arguments():
         assert res == [6, 7, 8]
 
 
-def test_parallel_for_dict_arguments():
-    for engine in [
-        "sequential",
-        "joblib",
-        # TODO fix me in CI
-        # "ray"
-    ]:
+def test_dict_arguments():
+    for engine in engines:
         res = parfor(
             f, [{"x": 1}, {"x": 2}, {"x": 3}], context=dict(v1=2, v2=3), engine=engine
         )
